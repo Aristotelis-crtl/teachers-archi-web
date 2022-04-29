bdb0596ade085e18d75efd5b3fc77d9242565913
Aristotelis-crtl <jabin.julian@gmail.com>
2022-04-29 11:19:11 +0200

    remove elements from sidebar if not logged

--- apps/teachers-archi-web/src/app/app.component.ts
+++ apps/teachers-archi-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '@prisma/client';
 
 @Component({
   selector: 'teachers-archi-web-root',
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isCollapsed = false;
   imageSrc = './assets/logoUPJV_Bleu.png';
+  isAdmin(): boolean {
+    const user = localStorage.getItem('user');
+    if (!user) {
+      return false;
+    }
+    const userParsed = JSON.parse(user) as User;
+    return userParsed.admin ? true : false;
+  }
+
+  isLoggedIn(): boolean {
+    const user = localStorage.getItem('user');
+    if (!user) {
+      return false;
+    }
+    return true;
+  }
 }
